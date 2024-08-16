a140214838786d0ea5d5509b19554c0eb853cb15
ggstudy11 <439403849@qq.com>
2024-08-16 09:58:41 +0800

    OrderPage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,3 @@
-<script setup></script>
-
 <template>
   <div>
     <SearchLine />
@@ -10,47 +8,73 @@
   <div class="mid">
     <span class="menu">今日菜单</span>
     <van-row gutter="0" style="margin: 2%">
-      <van-col span="6" class="menu-line">
-        <van-button class="menu-button"> 主食 </van-button>
-      </van-col>
-      <van-col span="6" class="menu-line">
-        <van-button class="menu-button"> 炒菜 </van-button>
-      </van-col>
-      <van-col span="6" class="menu-line">
-        <van-button class="menu-button"> 凉菜 </van-button>
-      </van-col>
-      <van-col span="6" class="menu-line">
-        <van-button class="menu-button"> 粥品 </van-button>
+      <van-col span="6" class="menu-line" v-for="button in buttons" :key="button.id">
+        <button
+          class="menu-button"
+          :class="{ focus: button.focus }"
+          @click="onClickMenuButton(button.name)"
+        >
+          {{ button.name }}
+        </button>
       </van-col>
     </van-row>
   </div>
   <div class="item-list">
-    <DishItem />
-    <DishItem />
-    <DishItem />
-    <DishItem />
-    <DishItem />
+    <DishItem v-bind="item" v-for="(item, id) in items" :key="id" />
   </div>
   <div>
     <router-link to="/login"></router-link>
     <router-link to="/OrderDetail"></router-link>
-    <router-link to="/ShoppingCart"></router-link>
   </div>
   <div>
     <van-action-bar>
       <div class="cart">
-        <van-action-bar-icon icon="cart-o" text="购物车" @click="onClickIcon" />
+        <van-action-bar-icon icon="cart-o" text="购物车" @click="onClickIcon"></van-action-bar-icon>
       </div>
       <div class="text">
         <span>查看购物车</span>
       </div>
       <div class="num">
-        <span>￥25.18</span>
+        <span>￥{{ count }}</span>
       </div>
     </van-action-bar>
   </div>
 </template>
 
+<script setup>
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+const router = useRouter()
+const count = ref('25.18')
+const buttons = [
+  { id: '1', name: '主食', focus: 'false' },
+  { id: '2', name: '炒菜', focus: 'false' },
+  { id: '3', name: '凉菜', focus: 'false' },
+  { id: '4', name: '粥品', focus: 'false' }
+]
+const items = [
+  { img: 'beef.png', name: '红烧肉', price: '2.00' },
+  { img: 'beef.png', name: '红烧肉', price: '2.00' },
+  { img: 'beef.png', name: '红烧肉', price: '2.00' },
+  { img: 'beef.png', name: '红烧肉', price: '2.00' },
+  { img: 'beef.png', name: '红烧肉', price: '2.00' },
+  { img: 'beef.png', name: '红烧肉', price: '2.00' }
+]
+const onClickIcon = () => {
+  router.push({ path: '/ShoppingCart' })
+}
+const onClickMenuButton = (name) => {
+  buttons.forEach((button) => {
+    if (button.name === name) {
+      button.focus = true
+    } else {
+      button.focus = false
+    }
+  })
+  console.log(name)
+  // 后续菜单的筛选逻辑在这里
+}
+</script>
 <style scoped>
 .search-line-icon {
   display: flex;
@@ -70,11 +94,17 @@
 }
 .menu-button {
   color: #ffa500;
-  border-color: #ffa500;
   height: 35px;
   width: 70px;
+  border: 1px solid #ffa500;
+  background-color: white;
+  border-radius: 7px;
+  font-size: medium;
 }
-
+.menu-button:focus {
+  background-color: orange;
+  color: black;
+}
 .menu-line {
   display: flex;
   align-items: center;
